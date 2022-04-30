d5d7cc3848312cd04bd4642cb243e708d1d6a3ab
rjsilvaoficial <rjsilvaoficial@gmail.com>
2022-04-30 10:49:00 -0300

    Projeto iniciado: criadas Models Course, Enrollment e Student, gerado crud scaffollding com customizações de controller e views para Student, criado contexto e classe DbInitializer, instanciado banco via EF

---
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,25 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
+
+        //Configurations to fluent API
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //}
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+    }
+}
---
+++ ContosoUniversity/Program.cs
@@ -0,0 +1,49 @@
+using ContosoUniversity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            CreateDbIfNotExists(host);
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using(var scope = host.Services.CreateScope()) //CreateScope disponibilizado por using DependencyInjection
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    //Obtém um contexto para viabilizar a interação com o banco de dados via ef
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error ocurred creating the DB.");
+                }
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
